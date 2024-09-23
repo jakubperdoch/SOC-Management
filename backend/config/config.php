1553d83ccbd23f59964055b7d49395fcdbc130fa
Jakub Perdoch <perdochjakub@gmail.com>
2024-09-23 13:47:15 +0200

    Add: composer

---
+++ backend/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+//konstanty pre pripojenie k databaze
+define('DB_CONNECTION', 'mysql');
+define('DB_HOST', 'mariadb105.r5.websupport.sk');
+define('DB_DATABASE', 'hz024702db');
+define('DB_USERNAME', 'hz024700');
+define('DB_PASSWORD', 'Dhynydor1');
+define('DB_CHARSET', 'utf8mb4');
+
--- backend/index.php
+++
@@ -1,1 +0,0 @@
-backend pico <3
